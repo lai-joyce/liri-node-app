fe163a3d38de00900a99a2e10f01be71609cf088
lai-joyce <lai.joyce@gmail.com>
2017-07-07 13:33:24 -0700

    draft 4

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require("request");
+var fs = require("fs");
 
 var apiKeys = require("./keys.js");
 
@@ -31,9 +32,9 @@ switch(params[2]) {
 
 	break;
 	
-case "spotify-this-song":
+	case "spotify-this-song":
 
-var song = "";
+	var song = "";
 
 // 	for (var i = 3; i < params.length; i++) {
 
@@ -47,7 +48,7 @@ song = params.slice(3).join(" ");
 if(!song) song = "The Sign Ace of Base";
 
 spotifyClient.search({ type: 'track', query: song }, function(err, data) {
-	// console.log(JSON.stringify(data, null, 2));
+	console.log(JSON.stringify(data, null, 2));
 	if ( err ) {
 		console.log('Error occurred: ' + err);
         return;  //from spotify npm docs
@@ -65,24 +66,47 @@ spotifyClient.search({ type: 'track', query: song }, function(err, data) {
 
 break;
 case "movie-this":
-var movieName = process.argv[3];
+// var movieName = process.argv[3];
+movie = params.slice(3).join(" ");
+if(!movie) movie = "Mr. Nobody";
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
 
-console.log(queryUrl);
+// console.log(queryUrl);
 
-// request(queryUrl, function(error, response, body) {
+request(queryUrl, function(error, response, body) {
+	// console.log(JSON.parse(body));
 
-//   if (!error && response.statusCode === 200) {
-  
-//   console.log("Release Year: " + JSON.parse(body).Year);
+	if (!error && response.statusCode === 200) {
 
-//   }
-// });
+		console.log("Movie: " + JSON.parse(body).Title);
+		console.log("Release Year: " + JSON.parse(body).Year);
+		console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+		console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+		console.log("Country Produced: " + JSON.parse(body).Country);
+		console.log("Language: " + JSON.parse(body).Language);
+		console.log("Plot: " + JSON.parse(body).Plot);
+		console.log("Actors: " + JSON.parse(body).Actors);
+
+	}
+});
 
 break;
 
 case "do-what-it-says":
+
+fs.readFile("random.txt", "utf8", function(error, data) {
+  
+  if (error) {
+    return console.log(error);
+  }
+  
+  console.log(data);
+  
+  // var dataArr = data.split(",");
+  
+  // console.log(dataArr);
+});
 
 break;
 }
