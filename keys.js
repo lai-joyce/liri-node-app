8a46ad2b57db9b31bddeaeaee87afa0b760b35bd
lai-joyce <lai.joyce@gmail.com>
2017-07-06 19:25:13 -0700

    draft 1

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'zdMCLwzqvY8n8E5ejGYYQ5zsf',
+  consumer_secret: 'NFPWNSWmZWK4sawj628LO7PNHxUF4czYVNfaShwBANXeiKSyQB',
+  access_token_key: '282899831-TjT7rdheU6sT7bZFA0bNBAK3MjsXgi1AKzeDnRfl',
+  access_token_secret: 'H75hmwuyo4eKwg2OmNz84a6XqiS23sg7NxTb06EiX48lT',
+}
+
+exports.spotifyKeys = {
+  id: 'faa76b1884c24ed58d98ef18f41ca11c',
+  secret: 'e4c6796a476c4c749a8d94970ecba812',
+}
---
+++ liri.js
@@ -0,0 +1,84 @@
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var require = require("request");
+
+var apiKeys = require("./keys.js");
+
+console.log(apiKeys);
+
+var twitterClient = new Twitter(apiKeys.twitterKeys);
+var spotifyClient = new Spotify(apiKeys.spotifyKeys);
+
+var params = process.argv
+
+switch(params[2]) {
+	case "my-tweets": 
+	twitterClient.get('statuses/user_timeline', { screen_name: 'dovesnunicorns', count: 20 }, 
+		function(error, tweets, response) {
+			if (!error) {
+				for (var i = 0; i < tweets.length; i++ ) {
+					console.log(tweets[i].text);
+					console.log(tweets[i].created_at)
+
+						// console.log(tweets);
+					}
+
+				}
+				else {
+					console.log("ERROR: " + error);
+				}
+			});
+
+	break;
+	
+case "spotify-this-song":
+
+var song = "";
+
+	for (var i = 3; i < params.length; i++) {
+
+  // Build a string with the arguments.
+  song = song + " " + params[i];
+
+}
+spotifyClient.search({ type: 'track', query: song }, function(err, data) {
+	// console.log(data.tracks.body);
+	if ( err ) {
+		console.log('Error occurred: ' + err);
+        return;  //from spotify npm docs
+    } else if (!song) {
+    	song = "Set Fire to the Rain";
+    }
+    else{
+    	var songInfo = data.tracks.items[0];
+    	var songResult = console.log(songInfo.artists[0].name)
+    	console.log(songInfo.name)
+    	console.log(songInfo.album.name)
+    	console.log(songInfo.preview_url)
+    // console.log(songResult);
+};
+});
+
+break;
+case "movie-this":
+var movieName = process.argv[3];
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+console.log(queryUrl);
+
+// request(queryUrl, function(error, response, body) {
+
+//   if (!error && response.statusCode === 200) {
+  
+//   console.log("Release Year: " + JSON.parse(body).Year);
+
+//   }
+// });
+
+break;
+
+case "do-what-it-says":
+
+break;
+}
