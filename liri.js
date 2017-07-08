970f82d626ab3d8f8ee2bfa07570fcca359ec335
lai-joyce <lai.joyce@gmail.com>
2017-07-08 15:50:09 -0700

    draft 5

--- liri.js
+++ liri.js
@@ -5,15 +5,51 @@ var fs = require("fs");
 
 var apiKeys = require("./keys.js");
 
-console.log(apiKeys);
+// console.log(apiKeys);
 
 var twitterClient = new Twitter(apiKeys.twitterKeys);
 var spotifyClient = new Spotify(apiKeys.spotifyKeys);
 
-var params = process.argv
+var params = process.argv;
 
-switch(params[2]) {
-	case "my-tweets": 
+// var action = process.argv[2];
+// var song = process.argv[3];
+
+// var runAction = function() {
+
+	switch(params[2]) {
+		case "my-tweets":
+
+		getTweets(); 
+
+		break;
+
+		case "spotify-this-song":
+
+		spotifyIt(params.slice(3).join(" "));
+
+		break;
+
+		case "movie-this":
+
+		grabMovieInfo();
+
+		break;
+
+		case "do-what-it-says":
+
+		followTheText();
+
+		break;
+	}
+
+// }
+
+
+
+
+function getTweets() {
+
 	twitterClient.get('statuses/user_timeline', { screen_name: 'dovesnunicorns', count: 20 }, 
 		function(error, tweets, response) {
 			if (!error) {
@@ -29,12 +65,15 @@ switch(params[2]) {
 					console.log("ERROR: " + error);
 				}
 			});
+}
 
-	break;
-	
-	case "spotify-this-song":
+function spotifyIt(song) {
 
-	var song = "";
+	// console.log(song);
+
+	// var song = "";
+	// console.log(arguments);
+
 
 // 	for (var i = 3; i < params.length; i++) {
 
@@ -43,34 +82,48 @@ switch(params[2]) {
 
 // }
 
-song = params.slice(3).join(" ");
-// console.log("song: " + song);
-if(!song) song = "The Sign Ace of Base";
+	
 
-spotifyClient.search({ type: 'track', query: song }, function(err, data) {
-	console.log(JSON.stringify(data, null, 2));
-	if ( err ) {
-		console.log('Error occurred: ' + err);
-        return;  //from spotify npm docs
-    }
+	// if(arguments[0]){
+	// 	song = arguments[0];
+	// } 
+	// else{
+		// song = params.slice(3).join(" ");
 
-    else{
-    	var songInfo = data.tracks.items[0];
-    	var songResult = console.log(songInfo.artists[0].name)
-    	console.log(songInfo.name)
-    	console.log(songInfo.album.name)
-    	console.log(songInfo.preview_url)
-    // console.log(songResult);
-};
-});
+		// console.log("params: " + params);
 
-break;
-case "movie-this":
-// var movieName = process.argv[3];
-movie = params.slice(3).join(" ");
-if(!movie) movie = "Mr. Nobody";
+		// console.log("this song is : " + song)
+		// console.log("song: " + song);
+		if(!song) song = "The Sign Ace of Base";
+	// }
+	// console.log(song)
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+	spotifyClient.search({ type: 'track', query: song }, function(err, data) {
+		// console.log(song, 'SONGSONGSONG')
+		// console.log('typeof', typeof song === typeof "The Sign Ace of Base")
+		// console.log(JSON.stringify(data, null, 2));
+		if ( err ) {
+			console.log('Error occurred: ' + err);
+	        return;  //from spotify npm docs
+	    }
+
+	    else{
+	    	var songInfo = data.tracks.items[0];
+	    	var songResult = console.log(songInfo.artists[0].name)
+	    	console.log(songInfo.name)
+	    	console.log(songInfo.album.name)
+	    	console.log(songInfo.preview_url)
+	    // console.log(songResult);
+	};
+	});
+}
+
+function grabMovieInfo() {
+	// var movieName = process.argv[3];
+	movie = params.slice(3).join(" ");
+	if(!movie) movie = "Mr. Nobody";
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
 
 // console.log(queryUrl);
 
@@ -90,23 +143,116 @@ request(queryUrl, function(error, response, body) {
 
 	}
 });
+}
 
-break;
+// function followTheText() {
+//      fs.readFile("random.txt", "utf8", function(error, data)
+//      {
+//      	// If an error was experienced we say it.
+//      	if(error){
+//      		console.log(error);
+//      	}
+//           else {
+//                var dataArray = data.split(',');
+//                var argOne = dataArray[0];
+//                var argTwo = dataArray[1];
+//                switch(argOne) {
+//                     case "my-tweets":
+//                          getTweets();
+//                          break;
+//                     case "spotify-this-song":
+//                        var queryInput = "the sign ace of base";
+//                        if (argTwo !== undefined){
+//                            queryInput = argTwo;
+//                        }
+                         // function spotifyIt() {
+                         //      var queryInput = "the sign ace of base";
+                         //      if (argTwo !== undefined){
+                         //           queryInput = argTwo;
+                         //      }
+                         //      console.log('TYPE', typeof queryInput);
+                         //      spotifyClient.search({ type: 'track', query: queryInput }, function(err, data) {
+                         //           if ( err ) {
+                         //                console.log('Error occurred: ' + err);
+                         //                return;
+                         //           } else {
+                         //           		console.log(data)
+                         //           }
 
-case "do-what-it-says":
+                         //           // console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                         //           // console.log("Song Name: " + data.tracks.items[0].name);
+                         //           // console.log("Spotify Preview Link: " + data.tracks.items[0].preview_url);
+                         //           // console.log("Album: " + data.tracks.items[0].album.name);
+                         //           // fs.appendFile('log.txt', "Artist: " + data.tracks.items[0].artists[0].name + "\n" + 
+                         //           // 	"Song Name: " + data.tracks.items[0].name + "\n" + "Spotify Preview Link: " + 
+                         //           // 	data.tracks.items[0].external_urls.spotify + "\n" + "Album: " + 
+                         //           // 	data.tracks.items[0].album.name + "\n" + "=================================================================");
+                         //      });
+                         // }
+                    //      spotifyIt(queryInput);
+                    //      break;
+                    // case "movie-this":
+                    //      function grabMovieInfo() {
+                    //           var queryInput = "Mr. Nobody";
+                    //           if (argTwo !== undefined) {
+                    //                queryInput = argTwo;
+                    //           }
+                    //           request('http://www.omdbapi.com/?t=' + queryInput + "&tomatoes=true", function (error, response, body) {
+                    //                if (!error && response.statusCode == 200) {
+                    //                     var movieData = JSON.parse(body);
+                    //                     console.log("Title: " + movieData.Title);
+                    //                     console.log("Year: " + movieData.Year);
+                    //                     console.log("IMDB Rating: " + movieData.imdbRating);
+                    //                     console.log("Country: " + movieData.Country);
+                    //                     console.log("Language: " + movieData.Language);
+                    //                     console.log("Plot: " + movieData.Plot);
+                    //                     console.log("Actors: " + movieData.Actors);
+                    //                     console.log("Rotten Tomatoes Rating: " + movieData.tomatoUserRating);
+                            		     // console.log("Rotten Tomatoes URL: " + movieData.tomatoURL);
+                                   //      fs.appendFile('log.txt', "Title: " + movieData.Title + "\n" + "Year: " + movieData.Year + "\n" + 
+                                   //      	"IMDB Rating: " + movieData.imdbRating + "\n" + "Country: " + movieData.Country + 
+                                   //      	"\n" + "Language: " + movieData.Language + "\n" + "Plot: " + movieData.Plot + "\n" + 
+                                   //      	"Actors: " + movieData.Actors + "\n" + "Rotten Tomatoes Rating: " + movieData.tomatoUserRating + "\n" + 
+                                   //      	"Rotten Tomatoes URL: " + movieData.tomatoURL + "\n" + "=================================================================");
+                         //           }
+                         //           else {
+                         //                console.log(error);
+                         //           }
+                         //      });
+                         // }
+//                          grabMovieInfo();
+//                          break;
+//                }
+//           }
+//      });
+// }
 
-fs.readFile("random.txt", "utf8", function(error, data) {
+function followTheText() {
+
+	fs.readFile("random.txt", "utf8", function(error, data) {
+
+		if (error) {
+			return console.log(error);
+		}
+
+		// console.log(data);
+
+		var dataArr = data.split(",");
+
+		// console.log(dataArr);
+
+  // console.log( "THis is my file reader: " + dataArr[1]);
+
+
+  		spotifyIt(dataArr[1]);
+  // if (dataArr[0] === "spotify-this-song") {
+  // 		spotifyIt(dataArr[1]);
+  // }
   
-  if (error) {
-    return console.log(error);
-  }
-  
-  console.log(data);
-  
-  // var dataArr = data.split(",");
-  
-  // console.log(dataArr);
+  // else {
+  // 		grabMovieInfo(dataArr[1]);
+  // }
+  		
+
 });
-
-break;
 }
